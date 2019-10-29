94b7d1fe5c8119320a9d814056daff88e98ce8e6
John Afolabi <johnafolabi.da@gmail.com>
2019-10-29 13:25:28 +0100

    Add Mouseover and Keydown events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// Mouseover
+const navItems = Array.from(document.getElementsByClassName('nav-link'));
+navItems.forEach(a => {
+    a.addEventListener('mouseover', event => {
+        event.currentTarget.style.color = '#fdbb4f';
+    })
+});
+
+// Keydown
+const body = document.getElementsByTagName('body')[0];
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'r') {
+        body.style.backgroundColor = '#ffebcd';
+        console.log(event)
+    }
+});
