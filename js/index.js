f461a25d4e6ad388f2749304d490b23869fe0930
John Afolabi <johnafolabi.da@gmail.com>
2019-10-29 14:10:25 +0100

    Add wheel and load events

--- js/index.js
+++ js/index.js
@@ -13,6 +13,19 @@ const body = document.getElementsByTagName('body')[0];
 document.addEventListener('keydown', event => {
     if (event.key === 'r') {
         body.style.backgroundColor = '#ffebcd';
-        console.log(event)
     }
+});
+
+// Wheel
+logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('wheel', event => {
+    event.target.textContent = 'Funny Bus';
+})
+
+// Drag & Drop 
+
+
+// Load
+window.addEventListener('load', event => {
+    alert('Page is fully loaded');
 });
