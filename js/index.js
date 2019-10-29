778fcdc0c99fefb95e6faf6cca34fe56bd9c8125
John Afolabi <johnafolabi.da@gmail.com>
2019-10-29 15:52:00 +0100

    Add submit, select, focus events

--- js/index.js
+++ js/index.js
@@ -40,3 +40,23 @@ const introText = document.getElementsByClassName('intro')[0].children[2];
 introText.addEventListener('dblclick', event => {
     event.target.style.backgroundColor = '#ffebcd';
 })
+
+// Submit
+const form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    alert('Form Submitted')
+})
+
+// Select
+const firstname = document.querySelector('.first-name');
+console.log(firstname);
+firstname.addEventListener('select', event => {
+   event.target.style.color = 'red'; 
+})
+
+// Focus
+firstname.addEventListener('focus', event => {
+    event.target.style.backgroundColor = 'yellow';
+})
+
